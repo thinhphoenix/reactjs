935e89a6bc04fe7b68ed4ad20a9650fbccf9ca3a
ThinhPhoenix <laithinh1310@gmail.com>
2025-12-19 00:25:56 +0700

    feat: update RouteComponent to include a link to /hello/1

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>fuck</div>;
+  return (
+    <div>
+      <Link to="/hello/1">Hello</Link>
+    </div>
+  );
 }
