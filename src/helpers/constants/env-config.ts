5657162e5edcccfbe6611b99f70ab7f1a726e5e6
ThinhPhoenix <laithinh1310@gmail.com>
2026-01-08 13:02:33 +0700

    fix: improve envConfig defaults and update layout in RouteComponent

--- src/helpers/constants/env-config.ts
+++ src/helpers/constants/env-config.ts
@@ -1,10 +1,10 @@
 export const envConfig = {
-  bunEnv: import.meta.env.PUBLIC_BUN_ENV,
+  bunEnv: import.meta.env.PUBLIC_BUN_ENV || 'development',
   base:
     import.meta.env.PUBLIC_BUN_ENV === 'production'
       ? import.meta.env.PUBLIC_BASE
       : '',
-  apiUrl: import.meta.env.PUBLIC_API,
-  apiTimeout: Number(import.meta.env.PUBLIC_API_TIMEOUT),
+  apiUrl: import.meta.env.PUBLIC_API || 'https://jsonplaceholder.typicode.com',
+  apiTimeout: Number(import.meta.env.PUBLIC_API_TIMEOUT) || 8000,
   port: Number(import.meta.env.PUBLIC_PORT) || 3000,
 };
